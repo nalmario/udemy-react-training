8fec691258b6fc2db507f3a614075d6da99d4fd9
nalmario <noahralmario@gmail.com>
2025-05-15 14:37:38 -0500

    completed to lesson 37 - handling side effects with useEffect()
    modified PostsLists.jsx by changing the post handler function to send new post data to a backend json file
    
    modified PostsLists.jsx to utilize the useEffect() hook to fetch existing post data in the .json file from the backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,12 +2,29 @@ import Post from './Post'
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 import Modal from './Modal'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function PostsList({isPosting, onStopPosting}) {
   const [posts, setPosts] = useState([]) // store list of posts, update when new post
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts')
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', { // port and location found in backend
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
