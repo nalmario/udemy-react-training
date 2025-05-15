d7650b0ab49dcb43683109e0f405cbac90583d16
nalmario <noahralmario@gmail.com>
2025-05-15 09:57:37 -0500

    completed to lesson 32 - handling form submission
    all state slices and functions for author and body text brought down to NewPost component
    
    submit handler function will handle post submission and store body and author values in a postData object

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,33 +2,18 @@ import Post from './Post'
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 import Modal from './Modal'
-import { useState } from 'react';
 
 function PostsList({isPosting, onStopPosting}) {
-  const [ enteredBody, setEnteredBody ] = useState('');
-  const [ enteredAuthor, setEnteredAuthor ] = useState('');
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
           <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
             onCancel={onStopPosting}
           />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody}/>
         <Post author="Manuel" body="Check out the full course!"/>
       </ul>
     </>
