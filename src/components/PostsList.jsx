8c7b304bb03623af4732dd841c5372c306ef81b0
nalmario <noahralmario@gmail.com>
2025-05-16 10:28:40 -0500

    completed to lesson 44 - data fethcing via loader()s
    Posts.jsx route now exports a loader function that will fetch json data from the dummy backend
    
    Postslist.jsx modified - posts variable uses useLoaderData hook to use data received from loader function
    
    main.jsx now has a loader property for the Posts element and has a value of the loader function

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,9 @@
 import Post from './Post';
 import classes from './PostsList.module.css';
-import { useState, useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function PostsList() {
-  const [posts, setPosts] = useState([]) // store list of posts, update when new post
-  const [ isFetching, setIsFetching ] = useState(false); // used for conditional rendering
-
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:8080/posts')
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData() // using this hook bc this is a nested component to Posts.jsx
 
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', { // port and location found in backend
@@ -32,20 +18,15 @@ function PostsList() {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && ( // conditional rendering
+      {posts.length > 0 && ( // conditional rendering
       <ul className={classes.posts}>
         {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
       </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{textAlign: 'center', color: 'white'}}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
